e76c224bebf760cb49e5be3b33690d0a775451f7
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-01 22:33:04 +0200

    Creating connection with MongoDB database

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ const PORT = process.env.PORT || 5000;
 
 const url = process.env.URL;
 const dbName = "Favorites";
+
+async function connectToDatabase() {
+  try {
+    const client = new MongoClient(url);
+    await client.connect();
+    console.log("Connected to the database");
+    return client.db(dbName);
+  } catch (err) {
+    console.error("Error connecting to the database", err);
+    throw err;
+  }
+}
+
+connectToDatabase();
