1d030a1dfe33d10fb08e4df1c44f9d7ebbecb9d5
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-01 22:31:55 +0200

    Defining the MongoDB connection url and database

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ const { MongoClient } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+
+const url = process.env.URL;
+const dbName = "Favorites";
