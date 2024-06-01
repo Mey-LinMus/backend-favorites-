e6130ca76e54e832ff3acc360f1f2feab17f803e
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-01 23:55:03 +0200

    Adding test if server deployment is working properly

--- server.js
+++ server.js
@@ -76,6 +76,10 @@ app.delete("/favorites/:deviceId/:favorite", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
