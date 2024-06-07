54856dd73c516fba2a589bce26c0bfa478b765e8
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-06-07 17:39:04 +0200

    Trying to fix error in frontend

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
-
 const app = express();
 require("dotenv").config();
-
 const PORT = process.env.PORT || 5000;
 const url = process.env.URL;
 const dbName = "WorkZen";
-
 async function connectToDatabase() {
   try {
     const client = new MongoClient(url);
@@ -52,18 +49,14 @@ app.post("/favorites/:deviceId", async (req, res) => {
 
 app.get("/favorites/:deviceId", async (req, res) => {
   const deviceId = req.params.deviceId;
-
   try {
     const db = await connectToDatabase();
     const favoritesCollection = db.collection("favorites");
-
     const favorite = await favoritesCollection.findOne({ deviceId });
-
     if (!favorite) {
       res.status(404).json({ message: "Favorite combination not found" });
       return;
     }
-
     res.json(favorite);
   } catch (err) {
     console.error(err);
